146b5445b6232d99d8a6ad354fb25bd675974dc2
JS Dev <DK1986830@gmail.com>
2019-09-28 23:45:53 +0200

    Revert "Jp/ticket 102"

--- frontend/src/components/corporateCreditFile/threeProduction/threeProduction.vue
+++ frontend/src/components/corporateCreditFile/threeProduction/threeProduction.vue
@@ -22,7 +22,7 @@
           </el-table-column>
           <el-table-column prop="cretficationCategory" label="认证类型" width="">
           </el-table-column>
-          <el-table-column prop="certificationNo" label="证书编号" width=""></el-table-column>
+          <el-table-column prop="serialNubmer" label="证书编号" width=""></el-table-column>
           <el-table-column label="证书有效期" width="">
             <template slot-scope="{row}">{{getCertification(row.certificationStartTime, row.certificationEndTime)}}</template>
           </el-table-column>
@@ -64,10 +64,10 @@ export default {
     };
   },
   created() {
-    this.id = this.$route.params.id;
     this.getProductionDetail();
     this.getCompanyProduction();
     this.getList();
+    this.id = this.$route.params.id;
   },
   methods: {
     getCompanyProduction() {
