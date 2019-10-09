f34c7d9581996655dd1d42a5b4bb3ed120a9fce8
white <whitecalm@outlook.com>
2019-10-09 22:41:18 +0200

    add files

---
+++ src/components/corporateCreditFile/corporateCreditFile.vue
@@ -0,0 +1,293 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">企业信用档案</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="box">
+      <div class="iptBox">
+        <div class="select_label">乡镇</div>
+        <el-select v-model="currTown" placeholder="请选择" @change="getList()">
+          <el-option v-for="item in township" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+        <div class="select_label">类型</div>
+        <el-radio v-model="bTypes" label="1" v-on:change="getList(true)">企业</el-radio>
+        <el-radio v-model="bTypes" label="2" v-on:change="getList(true)">农户</el-radio>
+        <div class="select_label">行政许可</div>
+        <el-select v-model="public_license" placeholder="请选择" @change="onSelectPubLicense">
+          <el-option
+            v-for="item in publicOptions"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
+        <div class="select_label">行政处罚</div>
+        <el-select v-model="public_punish" placeholder="请选择" @change="onSelectPubPunish">
+          <el-option
+            v-for="item in publicOptions"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
+        </el-select>
+        <div class="select_label">
+          <el-button type="outline-primary" v-on:click="getList()" disabled>导入</el-button>
+        </div>
+      </div>
+
+      <el-container>
+        <el-table
+          :data="tableData"
+          v-loading="listLoading"
+          fit
+          style="width: 100%;"
+          :row-class-name="rowIndex"
+          highlight-current-row
+        >
+          <el-table-column :formatter="order" label="序号" width="70"></el-table-column>
+          <el-table-column label="企业名称" width="150">
+            <template slot-scope="{row}">{{filterCompnay(row.creditCode)}}</template>
+          </el-table-column>
+          <el-table-column prop="creditCode" label="统一社会信用代码"></el-table-column>
+          <el-table-column prop="public_license" label="行政许可信息">
+            <template slot-scope="{row}">
+              <el-button v-if="row.public_license > 0"
+                v-on:click="$router.push({path: `/corporateCreditFile/adminLicenseInfo`,query: {creditCode:row.creditCode}})"
+              >行政许可信息</el-button>
+              <el-button v-else disabled>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;无&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column prop="public_punish" label="行政处罚信息">
+            <template slot-scope="{row}">
+              <el-button v-if="row.public_punish > 0"
+                v-on:click="$router.push({path: `/corporateCreditFile/adminPenaltyInfo`,query: {creditCode:row.creditCode}})"
+              >行政许可信息</el-button>
+              <el-button v-else disabled>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;无&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column label="评级信息">
+            <template slot-scope="{row}">
+              <span
+                class="rating-action"
+                v-on:click="$router.push({path: `/corporateCreditFile/ratingInfo`,query: {creditCode:row.creditCode}})"
+              >{{row.nowGrade}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="三品一标认证" width="200">
+            <template slot-scope="{row}">
+              <el-button
+                v-on:click="$router.push({path: `/corporateCreditFile/threeProduction`,query: {creditCode:row.creditCode}})"
+              >三品一标</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-container>
+      <div class="pageBox">
+        <pagination
+          v-show="total>0"
+          :total="total"
+          :page.sync="page.pageIndex"
+          :limit.sync="page.pageSize"
+          @pagination="getList"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Pagination from "@/components/common/pagination";
+import Request from "../../services/api/request.js";
+
+export default {
+  name: "creditRating",
+  components: { Pagination },
+  data() {
+    return {
+      township: [{ id: 0, name: "全部" }],
+      publicOptions: [
+        { id: 0, name: "全部" },
+        { id: 1, name: "有" },
+        { id: 2, name: "无" }
+      ],
+      currTown: 0,
+      public_license: 0,
+      public_punish: 0,
+      page: {
+        pageIndex: 1,
+        pageSize: 20
+      },
+      listLoading: true,
+      bTypes: 0,
+      status: 0,
+      total: 100,
+      tableData: [],
+      companyProduction: [],
+      gradData: []
+    };
+  },
+  mounted() {
+    this.getTown();
+    this.getList();
+    this.getCompanyProduction();
+  },
+  methods: {
+    rowIndex({ row, rowIndex }) {
+      row.rowIndex = rowIndex;
+    },
+    onSelectPubLicense() {
+      this.getList();
+    },
+    onSelectPubPunish() {
+      this.getList();
+    },
+    getCompanyProduction() {
+      Request()
+        .get("/api/company_production/name")
+        .then(response => {
+          this.companyProduction = response;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getTown() {
+      Request()
+        .get("/api/town/all")
+        .then(response => {
+          this.township = this.township.concat(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /* eslint-disable */
+    getList(update = false) {
+      this.listLoading = true;
+      Request()
+        .get("/api/company_production/all", {
+          companyType: this.bTypes,
+          approvalStatus: this.status - 1,
+          pageNo: this.page.pageIndex - 1,
+          pageSize: this.page.pageSize,
+          townId: this.currTown
+        })
+        .then(response => {
+          var tmpData = response;
+          if (this.public_license == 1) {
+            tmpData = tmpData.filter(function(licesnse) {
+              return licesnse.public_license > 0;
+            });
+          } else if (this.public_license == 2) {
+            tmpData = tmpData.filter(function(licesnse) {
+              return licesnse.public_license == 0;
+            });
+          }
+          if (this.public_punish == 1) {
+            tmpData = tmpData.filter(function(punish) {
+              return punish.public_punish > 0;
+            });
+          } else if (this.public_punish == 2) {
+            tmpData = tmpData.filter(function(punish) {
+              return punish.public_punish == 0;
+            });
+          }
+          this.tableData = [];
+          tmpData.map(item => {
+            this.getNowGrade(item.creditCode).then(res => {
+              item.nowGrade = this.getGradeString(res);
+              this.tableData.push(item);
+              console.log(this.tableData);
+            });
+          });
+          this.total = this.tableData.length;
+
+          setTimeout(() => {
+            this.listLoading = false;
+          }, 0.5 * 1000);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    /* eslint-enable */
+    filterCompnay(credit) {
+      let company = this.companyProduction.find(x => x.creditCode === credit);
+      if (company) {
+        return company.companyName;
+      } else {
+        return "";
+      }
+    },
+    async getNowGrade(creditCode) {
+      let nowGrade = "";
+      await Request()
+        .get("/api/company_credit_grade/all", {
+          approvalStatus: -1,
+          creditCode: creditCode,
+          // sortBy: 'creditGradeId DESC',
+          pageNo: 0,
+          pageSize: 20,
+          townId: 0
+        })
+        .then(response => {
+          if (!response || !response.length) nowGrade = "";
+          else nowGrade = response.pop().nowGrade;
+          return nowGrade;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      return nowGrade;
+    },
+    // getGrade(dataTable) {
+    //   let strGrade = "";
+    //   Request()
+    //     .get("/api/company_credit_grade/all", {
+    //       approvalStatus: this.status - 1,
+    //       creditCode: dataTable.creditCode,
+    //       pageNo: this.page.pageIndex - 1,
+    //       pageSize: this.page.pageSize,
+    //       townId: this.currTown
+    //     })
+    //     .then(response => {
+    //       this.gradData = response;
+    //       let nSIze = this.gradData.length;
+    //       strGrade = this.getGradeString(this.gradData[nSIze - 1].nowGrade);
+    //       dataTable.nowGrade = strGrade;
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //       return "";
+    //     });
+
+    //   return dataTable.nowGrade;
+    // },
+    getGradeString(grade) {
+      let strGrade = "";
+      switch (grade) {
+        case "A":
+          strGrade = "A级（守信）";
+          break;
+        case "B":
+          strGrade = "B级（基本守信）";
+          break;
+        case "C":
+          strGrade = "C级（失信）";
+          break;
+        default:
+          strGrade = "A级（守信）";
+      }
+      return strGrade;
+    },
+    order(row) {
+      return this.page.pageSize * (this.page.pageIndex - 1) + row.rowIndex + 1;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "./corporateCreditFile.scss";
+</style>
---
+++ src/components/sampleCheck/addSampleCheck/addSampleCheck.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/sampleCheck' }">例行抽样</el-breadcrumb-item>
+        <el-breadcrumb-item class="actived">添加计划</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="box">
+      <el-form v-if="data" :rules="dataRulse" ref="data" :model="data">
+        <el-form-item label="检测名称:" prop="sampleName">
+          <el-input v-model="data.sampleName" style="width: 300px;" placeholder=""></el-input>
+        </el-form-item>
+        <div class="item-row">
+          <div class="item">
+            <div class="item-label">检测时间:</div>
+            <div class="item-value">
+              <el-date-picker v-model="data.sampleTime" type="date" placeholder="date"></el-date-picker>
+            </div>
+          </div>
+          <div class="item">
+            <div class="item-label">检测人员:</div>
+            <el-form-item prop="checkPerson">
+              <el-input v-model="data.checkPerson" placeholder="" style="margin-top: 15px;"></el-input>
+            </el-form-item>
+          </div>
+        </div>
+        <div class="item-row">
+          <el-form-item prop="checkFiles">
+            <div style="display: flex;">
+              <div class="item-label">
+                <input type="file" id="file" style="display: none" ref="file"
+                  v-on:change="handleFileUpload()"/>
+                <el-button plain @click="chooseFile()">添加附件</el-button>
+              </div>
+              <span class="item-value" style="width: 500px;">{{data.checkFiles}}</span>
+            </div>
+          </el-form-item>
+        </div>
+        <div class="item-row">
+          <div class="item">
+            <div class="item-label"></div>
+            <div class="item-label">
+              <el-button plain @click="onSubmit()">保存</el-button>
+            </div>
+            <div class="item-value">
+              <el-button plain @click="$router.go(-1)">取消</el-button>
+            </div>
+          </div>
+        </div>
+      </el-form>
+      <template v-if="!data">No matching data!</template>
+    </div>
+  </div>
+</template>
+
+<script>
+import Request from '@/services/api/request'
+export default {
+  name: "addSampleCheck",
+  data() {
+    return {
+      id: -1,
+      file: null,
+      data: {
+        checkFiles: "",
+        checkPerson: "",
+        createUserId: 0,
+        id: 0,
+        sampleId: 0,
+        sampleName: "",
+        sampleTime: new Date(),
+        updateUserId: 0
+      },
+      dataRulse: {
+        sampleName: [{required: true, message: '请输入', trigger: 'blur,change'}],
+        checkPerson: [{required: true, message: '请输入', trigger: 'blur,change'}],
+        checkFiles: [{required: true, message: '请输入', trigger: 'blur,change'}],
+      },
+    };
+  },
+  methods: {
+    chooseFile() {
+      this.$refs.file.click();
+    },
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
+      this.data.checkFiles = this.file.name;
+    },
+    onSubmit() {
+      let formData = new FormData();
+      formData.append("checkFiles", this.data.checkFiles);
+      formData.append("checkPerson", this.data.checkPerson);
+      formData.append("createUserId", this.data.createUserId);
+      formData.append("id", this.data.id);
+      formData.append("sampleId", this.data.sampleId);
+      formData.append("sampleName", this.data.sampleName);
+      formData.append("updateUserId", this.data.updateUserId);
+      formData.append("file", this.file);
+      formData.append("sampleTime", this.data.sampleTime);
+      formData.append("checkResult", "");
+      formData.append("checkUnit", "");
+      formData.append("createTime", this.data.sampleTime); // not sure
+      formData.append("updateTime", this.data.sampleTime); // not sure
+      this.$refs.data.validate((valid) => {
+        if (valid) {
+          Request()
+            .post("/api/sample_check/create", formData)
+            .then(response => {
+              this.$router.push(`/sampleCheck`);
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        }
+      });
+    },
+    getDateString(dt) {
+      const date = new Date(dt);
+      return date.toLocaleDateString();
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "./addSampleCheck.scss";
+</style>
